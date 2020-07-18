ef824efb71b66f7cf9c9165a15cce3dbd6cca790
Jogius <julius@themakowskis.de>
2020-07-18 22:52:30 +0200

    Replaced ( with { to fix error message

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -27,7 +27,7 @@ export default function NewNote() {
         event.preventDefault();
 
         if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
-            alert(`Please pick a file smaller than $(config.MAX_ATTACHMENT_SIZE / 1000000} MB`);
+            alert(`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE / 1000000} MB`);
             return;
         }
         setIsLoading(true);
